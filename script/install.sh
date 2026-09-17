c390c58337409bd0f26ee9c2b643cd63f2f0ffb9
Donovan <donovan.montoya1@gmail.com>
2026-09-16 22:05:18 -0400

    fix: honor the last normalized installer setting

--- script/install.sh
+++ script/install.sh
@@ -423,9 +423,9 @@ config_value() {
       last = substr(value, length(value), 1)
       if (length(value) >= 2 && first == last && (first == "\"" || first == "\047"))
         value = substr(value, 2, length(value) - 2)
-      print value
-      exit
+      found = 1
     }
+    END { if (found) print value }
   ' "$config"
 }
 
